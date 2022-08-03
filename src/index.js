32509556832cd2c16cee93046be48972246975f4
olasrum <mursalo@gmail.com>
2022-08-03 18:03:04 +1000

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", () => { 
+
+  //By default, submit button is disabled
+  document.querySelector('#submit').disabled = true;
+
+  //When user finishes typing text > 0 then submit button is enabled
+  document.querySelector('#new-task-description').onkeyup = () => {
+    if (document.querySelector('#new-task-description').value.length > 0) {
+      document.querySelector('#submit').disabled = false;
+    } else {
+      document.querySelector('#submit').disabled = true;
+    } 
+  }
+
+  document.querySelector('form').onsubmit = () => {
+    const newTask = document.querySelector('#new-task-description').value;
+    
+    const li = document.createElement('li')
+    li.innerHTML = newTask;
+
+    document.querySelector('#tasks').append(li);
+
+    document.querySelector('#new-task-description').value = '';
+    document.querySelector('#submit').disabled = true;
+
+    //Stop form submitting
+    return false;
+  }
 });
+
+
+
